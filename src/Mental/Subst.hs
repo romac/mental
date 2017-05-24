4f8ebb0369ddbf006c0e5365c246d15f172e8655
Romain Ruetschi <romain.ruetschi@gmail.com>
2017-05-25 01:50:48 +0200

    WIP

--- src/Mental/Subst.hs
+++ src/Mental/Subst.hs
@@ -32,12 +32,12 @@ singleton k v = Substitution (Map.singleton k v)
 fromList :: [(TyName, Ty)] -> Substitution
 fromList = Substitution . Map.fromList
 
-biApply :: (Subst e, Bifunctor f) => Substitution -> f e e -> f e e
+biApply :: (Eq e, Subst e, Bifunctor f) => Substitution -> f e e -> f e e
 biApply s = bimap (subst s) (subst s)
 
 class Subst t where
   ftv   :: t -> Set TyName
-  subst :: Substitution -> t -> t
+  subst' :: Substitution -> t -> t
 
 hasFtv :: Subst t => TyName -> t -> Bool
 hasFtv n e = Set.member n (ftv e)
@@ -45,7 +45,7 @@ hasFtv n e = Set.member n (ftv e)
 instance Subst Ty where
   ftv = tyFtv
 
-  subst (Substitution s) = cata alg
+  subst' (Substitution s) = cata alg
     where
       alg ty@(TyVar n) = Map.findWithDefault (embed ty) n s
       alg ty           = embed ty
@@ -53,11 +53,17 @@ instance Subst Ty where
 instance Subst Scheme where
   ftv (Forall vars ty) = ftv ty \\ Set.fromList vars
 
-  subst (Substitution s) (Forall vars ty) =
+  subst' (Substitution s) (Forall vars ty) =
     Forall vars (subst s' ty)
       where s' = Substitution (foldr Map.delete s vars)
 
 instance Subst v => Subst (Map k v) where
   ftv m = foldMap (ftv . snd) (Map.toList m)
-  subst = Map.map . subst
+  subst' = Map.map . subst'
+
+subst :: (Eq t, Subst t) => Substitution -> t -> t
+subst sub = pfix (subst' sub)
+
+pfix :: Eq a => (a -> a) -> a -> a
+pfix f a = let b = f a in if a == b then b else pfix f b
 
